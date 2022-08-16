9d4aae5cbb256f7d587c95fa7a26a5a5d69f6c64
Omer Mordechai <81302085+OMER62@users.noreply.github.com>
2022-08-16 15:05:29 +0300

    Update FamilyRecipes.vue
    Done.Update

--- src/pages/FamilyRecipes.vue
+++ src/pages/FamilyRecipes.vue
@@ -22,6 +22,49 @@
                         </b-card-text>
                     </b-card>     
                 </b-card-group>
+                                  <b-card-group deck style="margin-top:-889px;margin-left: 350px;">
+                    <b-card no-body title="Pasta with tomato sauce" img-top tag="article" style="max-width: 20rem;" class="mb-2">
+                        <b-card-img src="https://img.mako.co.il/2020/05/07/shutterstock_388471528_b.jpg"/> 
+                        <b-card-title style="font-weight:bold; text-decoration:underline" title="Pasta with tomato sauce"></b-card-title>
+                        <b-card-text>
+                            <ul class="recipe-overview" style="list-style-type: none;">
+                                <li><span style="font-weight:bold; text-decoration:underline"> Recipe creator:</span> Omer's Sister</li>
+                                <li><span style="font-weight:bold; text-decoration:underline"> When do we make the recipe:</span> Every sunday </li>
+                                <li><span style="font-weight:bold; text-decoration:underline">Components:</span> A package of spaghetti,800 grams of crushed tomatoes,4 garlic cloves
+                                A teaspoon of dried oregano Basil stalk,A teaspoon and a half to 2 teaspoons of salt
+                                A spoonful of sugar,a liter of water</li>
+                                
+                                <li><span style="font-weight:bold; text-decoration:underline">Instructions:</span>
+                                <p><small>1</small> כותשים את שיני השום.</p>
+                                <p><small>2</small> שמים את כל המרכיבים בסיר ומביאים לרתיחה.</p>
+                                <p><small>3</small> מבשלים 10 דקות תוך בעבוע על אש גבוהה, ומגישים.</p>                                </li>
+                            </ul> 
+                        </b-card-text>
+                    </b-card>     
+                </b-card-group>
+                  <b-card-group deck style="margin-top:-699px;margin-left: 750px;">
+                    <b-card no-body title="Pasta with tomato sauce" img-top tag="article" style="max-width: 20rem;" class="mb-2">
+                        <b-card-img src="https://img.mako.co.il/2020/09/01/master_chef3_2_maroken_fish_i.jpg"/> 
+                        <b-card-title style="font-weight:bold; text-decoration:underline" title="Moroccan fish Recipe"></b-card-title>
+                        <b-card-text>
+                            <ul class="recipe-overview" style="list-style-type: none;">
+                                <li><span style="font-weight:bold; text-decoration:underline"> Recipe creator:</span> Omer's Father</li>
+                                <li><span style="font-weight:bold; text-decoration:underline"> When do we make the recipe:</span> Every Firday dinner </li>
+                                <li><span style="font-weight:bold; text-decoration:underline">Components:</span> 1 sweet red pepper,4-5 ripe tomatoes,A head of garlic broken into cloves,
+                                sweet paprika,3 hot dry red peppers
+                               4-5 pieces of stretched fish - boneless fillet,Canola oil</li>
+                                
+                                <li><span style="font-weight:bold; text-decoration:underline">Instructions:</span>
+                                <p><small>1</small> מחממים מעט שמן בסיר, חותכים את הפלפל המתוק לרצועות דקות ומטגנים. פורסים את העגבניות לפרוסות דקות ומוסיפים לסיר.</p>
+                                <p><small>2</small>מקלפים את השום, מוסיפים לסיר ומנמיכים את האש.</p>
+                                <p><small>3</small>מוסיפים רבע מכמות הכוסברה, חצי כפית פפריקה ורבע כפית כורכום.מוסיפים חצי כוס מים ואת הפלפל החריף ומבשלים על אש נמוכה במכסה סגור.</p>
+                                <p><small>4</small>בקערה נפרדת מערבבים כוס שמן קנולה עם כף גדושה של פפריקה, חצי כפית כורכום, כף מלח והכוסברה שנותרה, עד לקבלת תערובת אחידה.</p>
+                                <p><small>5</small>בקערה נפרדת מערבבים כוס שמן קנולה עם כף גדושה של פפריקה, חצי כפית כורכום, כף מלח והכוסברה שנותרה, עד לקבלת תערובת אחידה.</p>
+                                </li>
+                            </ul> 
+                        </b-card-text>
+                    </b-card>     
+                </b-card-group>
             </section>
         </div>
     </div>
