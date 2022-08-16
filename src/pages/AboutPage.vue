ee88bd52f3b93b34266b563da82aa3547252988f
LIDORZ55 <lidorzurzur@gmail.com>
2022-08-16 18:23:48 +0300

    updated about page

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -7,7 +7,7 @@
           <b-list-group-item>Student: Lidor Zur</b-list-group-item>
           <b-list-group-item>The site was built as part of a Web Developer course   </b-list-group-item>
           <b-list-group-item>The purpose of the site is to provide the user with a system through which he can search for different recipes, save different recipes and create and share recipes</b-list-group-item>
-          <b-list-group-item>Additional Project:</b-list-group-item>
+          <b-list-group-item>Additional Project: https://github.com/Web-Development-Environments-2022/314881723_212185540</b-list-group-item>
         </b-list-group>
       </b-card>
   </div>
